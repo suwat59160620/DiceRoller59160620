03647938120f7452c4e7c742d5fd5ab1f3e4bee1
kritinee <59160839@go.buu.ac.th>
2019-08-07 17:34:32 +0700

    commit1

---
+++ app/src/main/java/buu/informatics/s59160620/diceroller/MainActivity.kt
@@ -0,0 +1,15 @@
+package buu.informatics.s59160620.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+    }
+}
+
