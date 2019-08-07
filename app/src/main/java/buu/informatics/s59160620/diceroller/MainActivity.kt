7531a071135c4ad6cce5f82e6891375d693a294a
kritinee <59160839@go.buu.ac.th>
2019-08-07 18:23:58 +0700

    commit2

--- app/src/main/java/buu/informatics/s59160620/diceroller/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160620/diceroller/MainActivity.kt
@@ -3,13 +3,39 @@ package buu.informatics.s59160620.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+//import kotlin.random.Random
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val rollButton: Button = findViewById(R.id.roll_button)
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice Rolled!"
+        rollButton.setOnClickListener { rollDice() }
+
+        val reButton: Button = findViewById(R.id.re_button)
+        reButton.setOnClickListener{ reset() }
+
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
+        val randomInt = Random().nextInt(6) + 1
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+    }
+
+    private fun reset() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "0"
     }
 }
 
